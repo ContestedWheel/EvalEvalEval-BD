891614d4d920070f3ef742509d73ab20eabe1f04
ContestedWheel <ajp0013@yahoo.com>
2025-07-11 23:43:51 -0400

    Update send_spawn_art.py

--- Evals/Balls/send_spawn_art.py
+++ Evals/Balls/send_spawn_art.py
@@ -1,3 +1,3 @@
 ball = await Ball.get(country="name here")
-file_location = "." + ball.wild_card
+file_location = "./admin_panel/media/" + ball.wild_card
 await ctx.send(file=discord.File(file_location))
