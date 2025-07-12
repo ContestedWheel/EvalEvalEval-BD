7736bc0c7a05056a3776482254847dfcd18c1f1e
ContestedWheel <ajp0013@yahoo.com>
2025-07-12 03:26:02 -0400

    Update change_enabled.py

--- Evals/Updating/change_enabled.py
+++ Evals/Updating/change_enabled.py
@@ -1,3 +1,9 @@
 ball = await Ball.get(country="COUNTRY NAME HERE")
 ball.enabled = True/False
 await ball.save()
+
+# If true:
+# Ball will show up in completion and spawn if rarity is not 0.
+
+# If false:
+# Ball will not show up in completion.
