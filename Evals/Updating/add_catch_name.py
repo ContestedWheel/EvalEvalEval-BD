49d054f781282011c91bf8982d6b1aaae5a44d63
ContestedWheel <ajp0013@yahoo.com>
2025-07-12 03:21:17 -0400

    Update add_catch_name.py

--- Evals/Updating/add_catch_name.py
+++ Evals/Updating/add_catch_name.py
@@ -1,3 +1,6 @@
 ball = await Ball.get(country="COUNTRY NAME")
 ball.catch_names = ball.catch_names + ";NEW CATCH NAME"
 await ball.save()
+
+# The semicolon is not a typo, it's there to make the catch name valid (example: catchname1;catchname2). Otherwise, it will look like this in the panel and not work:
+# catchname1catchname2
