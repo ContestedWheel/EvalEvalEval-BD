b3cba7690d2f338b04e169dc58b7a7d4e89113cf
ContestedWheel <ajp0013@yahoo.com>
2025-07-12 03:27:34 -0400

    Update change_short_name.py

--- Evals/Updating/change_short_name.py
+++ Evals/Updating/change_short_name.py
@@ -1,3 +1,5 @@
 b.eval ball = await Ball.get(country="COUNTRY NAME HERE.")
 ball.short_name = "SHORT NAME HERE"
 await ball.save()
+
+# The short name is the text that appears on top of the card that is next to the economy icon.
