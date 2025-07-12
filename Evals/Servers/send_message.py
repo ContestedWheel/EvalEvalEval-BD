5fd6e7a1ce3bb1467d2919a01da45f9846dde526
ContestedWheel <ajp0013@yahoo.com>
2025-07-12 03:10:17 -0400

    Update send_message.py

--- Evals/Servers/send_message.py
+++ Evals/Servers/send_message.py
@@ -1,6 +1,6 @@
-# Same channel
+# Use this one if you want to send a message in the same server
 await ctx.send("text")
 
-# Different channel
+# Use this one if you want to send a message in a different channel.
 channel = bot.get_channel(channel id here)
 await channel.send('text') 
