1d7526f10229aeb8b97c976626741372bca67020
ContestedWheel <ajp0013@yahoo.com>
2025-08-09 01:04:43 -0400

    Create owns_most_specific_ball.py

---
+++ Evals/Balls/owns_most_specific_ball.py
@@ -0,0 +1,11 @@
+from tortoise.functions import Count
+
+BALL_NAME = "INSERT_BALL_NAME_HERE"
+players = await Player.annotate(
+    ball_count=Count("balls", distinct=True)
+).filter(ballsballcountry=BALL_NAME).order_by("-ball_count").limit(INSERT_AMOUNT_OF_USERS)
+
+print(BALL_NAME + "\n")
+for player in players:
+    discord_user = await bot.fetch_user(int(player.discord_id))
+    print(f"{player.ball_count}: {discord_user}")
