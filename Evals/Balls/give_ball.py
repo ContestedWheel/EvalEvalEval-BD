87b46567629690aa05b76a9b608043ad7c7af886
ContestedWheel <ajp0013@yahoo.com>
2025-08-10 11:38:18 -0400

    Create give_ball.py

---
+++ Evals/Balls/give_ball.py
@@ -0,0 +1,19 @@
+If you want the balls to have random stats, use this command:
+
+.eval
+import random
+from ballsdex.core.models import Ball, Player, Special, BallInstance
+from ballsdex.settings import settings
+
+ball = await Ball.get(country="INSERT_COUNTRY")
+player = await Player.get(discord_id=INSERT_DISCORD_ID)
+special = await Special.get(name="INSERT_SPECIAL") # Set to "None" if you don't want a Special.
+shiny = INSERT_True_OR_False
+atk = random.randint(-20, 20)
+hp = random.randint(-20, 20)
+amount = INSERT_NUMBER
+
+for i in range(1):
+    await BallInstance.create(ball=ball, player=player, shiny=shiny, attack_bonus=atk, health_bonus=hp, special=special)
+
+await ctx.send(f"Successfully given <@{player.discord_id}> {ball} {amount} times!")
