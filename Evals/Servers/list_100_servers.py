7d9e4044c9ca711b1de0aaa47d2139e019c5821d
ContestedWheel <ajp0013@yahoo.com>
2025-08-12 00:48:00 -0400

    Update and rename list_all_servers.py to list_100_servers.py

--- Evals/Servers/list_100_servers.py
+++ Evals/Servers/list_100_servers.py
@@ -2,4 +2,5 @@ sorted_guilds = sorted(bot.guilds, key=lambda guild: guild.member_count, reverse
 top_10_guilds = sorted_guilds[:100]
 
 for guild in top_10_guilds:
+
     await ctx.send(f"{guild.name} - Member Count: {guild.member_count} - ID:{guild.id}")
