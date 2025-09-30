6e26cc1e26b7eb43d4a47b3a51653e7732490fc8
ContestedWheel <ajp0013@yahoo.com>
2025-09-30 13:23:56 -0400

    Update daily.py

--- Custom Commands/daily/daily.py
+++ Custom Commands/daily/daily.py
@@ -82,8 +82,8 @@ TIMEZONE_SETTING = timezone(timedelta(hours=8)) # Timezone configuration - chang
             instance = await BallInstance.create(
                 ball=ball,
                 player=player,
-                attack_bonus=random.randint(settings.max_attack_bonus, settings.max_attack_bonus),
-                health_bonus=random.randint(settings.max_health_bonus, settings.max_health_bonus),
+                attack_bonus=random.randint(-settings.max_attack_bonus, +settings.max_attack_bonus),
+                health_bonus=random.randint(-settings.max_health_bonus, +settings.max_health_bonus),
             )
             
             self.daily_claims[user_id] = now
